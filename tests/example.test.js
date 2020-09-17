51be650a2091ff55642ae28648e9c5aa6a6ba348
petersolimine <psolimine@tulane.edu>
2020-09-17 12:29:41 -0500

    fix bug litttyyyy

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
+
 describe('my first puppeteer test', () =>{ 
-    it('should launch the browser'), async function() {
+    it('should launch the browser', async function() {
         const browser = await puppeteer.launch({
             headless: false,
             slowMo: 10,
@@ -9,6 +10,15 @@ describe('my first puppeteer test', () =>{
         });
         const page = await browser.newPage();
         await page.goto('http://www.example.com');
+        const title = await page.title();
+        const url = await page.url();
+        const text = await page.$eval('h1', element => element.textContent);
+        const count = await page.$$eval('p', element => element.length);
+        await page.waitFor(3000);
+        console.log('number of p tags on page ' + count );
+        console.log(' text in h1: ' + text);
+        await browser.close()
 
-    }
-})
+
+    })
+});
